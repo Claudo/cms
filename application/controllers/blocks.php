436ddfe7e03f50e36cbf60ad616d181938c69609
kirill-petrovi4 <kirill.petrovi4@gmail.com>
2013-06-10 15:48:12 +0400

    исправление коммита

--- application/controllers/blocks.php
+++ application/controllers/blocks.php
@@ -1,16 +1,13 @@
 <?php
 class Blocks_Controller extends Base_Controller {
     public  function action_index() {
-<<<<<<< HEAD
         $view = View::make('blocks.home')->with('navActive', 'blocks');
-=======
 
         $blocks = Blocks::getAll();
 
         $view = View::make('blocks.home')
                         ->with('navActive', 'blocks')
                         ->with('blocks', $blocks);
->>>>>>> 4b211cc633c6b68c3d12ca30f1ab7744c23cf6f0
 
         return $view;
     }
